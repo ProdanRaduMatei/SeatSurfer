e2546277e29d0d95f7cf22a7b8cb234b5f8c32d1
atamas <atamas@sdl.com>
2024-08-06 14:13:47 +0300

    Fix indentation issues

--- src/main/java/org/seatsurfer/controller/UsersController.java
+++ src/main/java/org/seatsurfer/controller/UsersController.java
@@ -12,34 +12,34 @@ import java.util.Optional;
 @RestController
 @RequestMapping("api/users")
 public class UsersController {
-        @Autowired
-        private UsersServices usersServices;
+    @Autowired
+    private UsersServices usersServices;
 
-        @GetMapping
-        public List<Users> getAllUsers() {
-                return usersServices.getAllUsers();
-        }
+    @GetMapping
+    public List<Users> getAllUsers() {
+        return usersServices.getAllUsers();
+    }
 
-        @GetMapping("/{id}")
-        public ResponseEntity<Users> getUserById(@PathVariable Long id) {
-            Optional<Users> user = usersServices.getUserById(id);
-            return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-        }
+    @GetMapping("/{id}")
+    public ResponseEntity<Users> getUserById(@PathVariable Long id) {
+        Optional<Users> user = usersServices.getUserById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
-        @PostMapping
-        public Users createUser(@RequestBody Users user) {
-            return usersServices.createUser(user);
-        }
+    @PostMapping
+    public Users createUser(@RequestBody Users user) {
+        return usersServices.createUser(user);
+    }
 
-        @PutMapping("/{id}")
-        public ResponseEntity<Users> updateUser(@PathVariable Long id, @RequestBody Users userDetails) {
-            Users user = usersServices.updateUser(id, userDetails);
-            return ResponseEntity.ok(user);
-        }
+    @PutMapping("/{id}")
+    public ResponseEntity<Users> updateUser(@PathVariable Long id, @RequestBody Users userDetails) {
+        Users user = usersServices.updateUser(id, userDetails);
+        return ResponseEntity.ok(user);
+    }
 
-        @DeleteMapping("/{id}")
-        public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-            usersServices.deleteUser(id);
-            return ResponseEntity.noContent().build();
-        }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        usersServices.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
